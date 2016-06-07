74515a24ac37116f42b069ade1890e8a0e5357b4
Steven Preston <stevenpreston@stellar16.com>
2016-06-07 09:24:40 -0400

    Adds header
    - Adds header template with nav for register, log in, log out
    - Updates session helper with logged_in? method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,10 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def logged_in?
+    current_user
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
